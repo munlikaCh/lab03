996f6ec2d478ce8eb2e23b222e988b306e1afda8
munlikaCh <munlika_ch@cmu.ac.th>
2021-07-18 22:39:29 +0700

    5.8

--- lab-vue/src/views/EventList.vue
+++ lab-vue/src/views/EventList.vue
@@ -1,6 +1,6 @@
 <template>
-<h1>Events For Good</h1>
   <div class="events">
+    <h1>Events For Good</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <ShowCo class="shows" v-for="show in events" :key="show.id" :show="show" />
   </div>
@@ -10,6 +10,8 @@
 // import HelloWorld from "@/components/HelloWorld.vue";
 import EventCard from '../components/EventCard.vue'
 import ShowCo from '../components/ShowCo.vue'
+
+import axios from 'axios'
 
 
 export default {
@@ -21,42 +23,18 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          id: 5928101,
-          category: 'animal welfare',
-          title: 'Cat Adoption Day',
-          description: 'Find your new feline friend at this event.',
-          location: 'Meow Town',
-          date: 'January 28, 2022',
-          time: '12:00',
-          petsAllowed: true,
-          organizer: 'Kat Laydee'
-        },
-        {
-          id: 4582797,
-          category: 'food',
-          title: 'Community Gardening',
-          description: 'Join us as we tend to the community edible plants.',
-          location: 'Flora City',
-          date: 'March 14, 2022',
-          time: '10:00',
-          petsAllowed: true,
-          organizer: 'Fern Pollin'
-        },
-        {
-          id: 8419988,
-          category: 'sustainability',
-          title: 'Beach Cleanup',
-          description: 'Help pick up trash along the shore.',
-          location: 'Playa Del Carmen',
-          date: 'July 22, 2022',
-          time: '11:00',
-          petsAllowed: false,
-          organizer: 'Carey Wales'
-        }
-      ]
+      events: null
     }
+  },
+  created(){
+      axios
+      .get('http://localhost:3004/events')
+      .then((response) => {
+        this.events = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 }
 </script>
